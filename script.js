82ec4d86f4f554a0cd4e0b8ef66abd3bac0c5d01
davidnogueiradossantos951-gif <davidnogueiradossantos951@gmail.com>
2026-09-09 23:11:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function entrarNaHistoria() {
         behavior: "smooth"
     });
 }
+function entrarNaHistoria() {
+    document.getElementById("historia").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function controlarMusica() {
+    const musica = document.getElementById("musica");
+    const botao = document.getElementById("botao-musica");
+
+    if (musica.paused) {
+        musica.play();
+        botao.innerHTML = "🔊";
+    } else {
+        musica.pause();
+        botao.innerHTML = "🎵";
+    }
+}
